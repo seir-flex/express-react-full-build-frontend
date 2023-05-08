d41ca69c6c451cce49f82ee1954913c5144b339b
GA-DAVID <david.acosta@generalassemb.ly>
2023-05-08 13:31:58 -0700

    okidoki artichokie. Made an UpdatePerson page component

---
+++ src/pages/UpdatePerson.js
@@ -0,0 +1,13 @@
+import React from 'react'
+import './UpdatePerson.css'
+		  
+function UpdatePerson() {
+    return (
+		<div className="updateperson">
+					  
+		</div>
+	)
+}
+		  
+export default UpdatePerson
+		  
